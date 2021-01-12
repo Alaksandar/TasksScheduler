8ad1c7a0b15855db2b406b08ad7f26828e73ba49
alaksandar_km <ebenzomarket@gmail.com>
2021-01-12 02:39:32 +0100

    working code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,57 @@
 
+let buttonsContainer = document.querySelector(".buttons-container")
+let buttons = document.querySelectorAll("button");
+let list = document.querySelector(".list-container");
+
+
+buttonsContainer.addEventListener("click", selectActiveBtn);    // делегирование
+// container.addEventListener("click", showList);    
+
+
+    function selectActiveBtn(e) {
+
+        let button = e.target.closest("button");       // делегирование
+        if ( (button == null) ||
+            (!button.classList.contains("active"))) return;
+
+        for (let btn of buttons) {
+            btn.classList.toggle("active");     // переключатель class="active" между кнопками
+
+            let dataClass =  btn.dataset.action;    // запись значений из 
+            // console.log(dataClass);              // button[data] в 
+            list.classList.toggle(dataClass);       // расширение класса div 
+            if (!dataClass) return;                 //
+            let elems = document.getElementsByClassName(dataClass);
+            // console.log(elems);                  // нельзя воспользоваться
+            for (let el of elems) {                 // querySelector ?
+                // console.log(el)
+                if (!el) return;
+            }
+        }
+    }
+
+
+
+
+
+
+// for (let btn of buttons) {
+
+//     if (btn.classList.contains("active")) {
+//         this.addEventListener("click", btnActive);
+//         this.addEventListener("click", showList);
+// 	}
+//}
+
+//     function btnActive() {
+//         for (let btn of buttons) {
+//             btn.classList.toggle("active");
+//         }
+//     }
+
+//     function showList() {
+//         list.classList.toggle("show");
+//         list.classList.toggle("hide");
+//     }
+
+// // 
