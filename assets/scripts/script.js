117daa223ab2fc8993d876a73c283db998ef437d
alaksandar_km <ebenzomarket@gmail.com>
2021-02-21 20:14:30 +0100

    commit before add webpack

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,54 +4,24 @@ let buttons = document.querySelectorAll("button");
 let list = document.querySelector(".list-container");
 
 
-buttonsContainer.addEventListener("click", selectActiveBtn);    // делегирование
-// container.addEventListener("click", showList);    
+buttonsContainer.addEventListener("click", selectActiveBtn);
+ 
 
+function selectActiveBtn(e) {
 
-    function selectActiveBtn(e) {
+    let button = e.target.closest("button");
+    if ( (button == null) ||
+        (!button.classList.contains("active"))) return;
 
-        let button = e.target.closest("button");       // делегирование
-        if ( (button == null) ||
-            (!button.classList.contains("active"))) return;
+    for (let btn of buttons) {
+        btn.classList.toggle("active");
 
-        for (let btn of buttons) {
-            btn.classList.toggle("active");     // переключатель class="active" между кнопками
-
-            let dataClass =  btn.dataset.action;    // запись значений из 
-            // console.log(dataClass);              // button[data] в 
-            list.classList.toggle(dataClass);       // расширение класса div 
-            if (!dataClass) return;                 //
-            let elems = document.getElementsByClassName(dataClass);
-            // console.log(elems);                  // нельзя воспользоваться
-            for (let el of elems) {                 // querySelector ?
-                // console.log(el)
-                if (!el) return;
-            }
+        let dataClass =  btn.dataset.action;
+        list.classList.toggle(dataClass);
+        if (!dataClass) return;
+        let elems = document.getElementsByClassName(dataClass);
+        for (let el of elems) {
+            if (!el) return;
         }
     }
-
-
-
-
-
-
-// for (let btn of buttons) {
-
-//     if (btn.classList.contains("active")) {
-//         this.addEventListener("click", btnActive);
-//         this.addEventListener("click", showList);
-// 	}
-//}
-
-//     function btnActive() {
-//         for (let btn of buttons) {
-//             btn.classList.toggle("active");
-//         }
-//     }
-
-//     function showList() {
-//         list.classList.toggle("show");
-//         list.classList.toggle("hide");
-//     }
-
-// // 
+}
