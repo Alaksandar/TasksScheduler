38d37bb2be277ad235f4eea635efab5145053659
alaksandar_km <ebenzomarket@gmail.com>
2021-02-23 04:14:54 +0100

    try more to fix localStorage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,102 +6,177 @@ let list = document.querySelector(".list-container");
 let li_list = document.getElementsByTagName("li");
 let ul_list = document.querySelector(".list-container ul");
 
-let form = document.querySelector("#form1");
-let textInput = form.querySelector('[type="text"]');
-let submit = form.querySelector('[type="submit"]');
+let form = document.querySelector("form");
+let textInput = form.querySelector('input[type="text"]');
+let resetInput = document.querySelector('input[type=reset]');
+let submit = form.querySelector('input[type="submit"]');
 
 
 
-buttonsContainer.addEventListener("click", showList);
- 
-function showList(e) {
-    
-    let button = e.target.closest("button");
-    
-    if ( (button == null) ||
-        (!button.classList.contains("active"))) return;
-        
-    for (let btn of buttons) {
-        
-        btn.classList.toggle("active");
-        let dataClass =  btn.dataset.action;    
-        if (!dataClass) return;
-        for (let li of li_list) { 
-            if (li.children[0].checked) {
-                li.classList.toggle(dataClass);
+class ShowHideBtn {
 
+    constructor(name) {
+        this.name = name;
+    }
+
+    showList(e) {
+
+        let button = e.target.closest("button");
+
+        if ((button == null) ||
+            (!button.classList.contains("active"))) return;
+
+        for (let btn of buttons) {
+
+            btn.classList.toggle("active");
+            let dataClass = btn.dataset.action;
+            if (!dataClass) return;
+            for (let li of li_list) {
+                if (li.children[0].checked) {
+                    li.classList.toggle(dataClass);
+
+                }
             }
         }
     }
 }
 
+const showHideBtn = new ShowHideBtn();
 
-// form.addEventListener("submit", getValueOnSubmit);
-// form.addEventListener("submit", createToDoLi);
-// form.addEventListener("submit", toLocalStorage);
+buttonsContainer.addEventListener("click", ev => {
+    showHideBtn.showList(ev);
+})
 
-form.addEventListener("submit", formOnSubmit)
 
-function formOnSubmit(e) {
+class CreateNewTask {
+
+    constructor(name, checked = false) {
+        this.name = name;
+        this.checked = checked;
+    }
+
+
+    getValueOnSubmit() {
+
+        const formData = new FormData(form);
+        let value = Object.fromEntries(formData.entries());
+        if (!value.toList) return;
+        textInput.value = "";
+
+        this.render(value.toList);
+    }
+
+
+    render(value) {
+
+        let li = document.createElement("li");
+        let x = li_list.length;
+
+        let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.checked = this.checked;
+        checkbox.id = `${x}`;
+
+        let label = document.createElement('label');
+        label.textContent = value;
+        label.htmlFor = `${x}`;
+
+
+        // //  не работает:
+
+        // if (!li.previousElementSibling) {
+        //     checkbox.setAttribute('id',  0);
+        //     label.setAttribute('for',  0);
+        // } else {
+        //     checkbox.setAttribute('id', +li.previousElementSibling.firstElementChild.getAttribute('id') + 1);
+        //     label.setAttribute('for', +checkbox.getAttribute('id'));
+        // }
+
+        let deleteIcon = document.createElement("img");
+        deleteIcon.setAttribute('src', './assets/images/icon-close.png');
+        deleteIcon.addEventListener('click', remove => {
+            li.remove();
+            this.toLocalStorage();
+        });
+
+        li.append(checkbox);
+        li.append(label);
+        li.append(deleteIcon);
+
+        ul_list.append(li);
+
+
+        this.toLocalStorage();
+    }
+
+
+    changeHandler(e) {
+        if (e.target.tagName != 'INPUT') {
+            return
+        } else {
+            if (e.target.checked) {
+                e.target.setAttribute('checked', true);
+            } else {
+                e.target.removeAttribute('checked')
+            }
+            console.log(e.target.checked);
+        }
+        this.toLocalStorage();
+    }
+
+    reload() {
+        if (localStorage.getItem("todoes")) {
+            ul_list.innerHTML = localStorage.getItem("todoes");
+        }
+    }
+
+    toLocalStorage() {
+        let todoes = ul_list.innerHTML;
+        localStorage.setItem('todoes', todoes);
+
+
+    //     const config = {
+    //         attributes: true,
+    //         childList: true,
+    //         subtree: true,
+    //         characterData: true
+    //     };
+    //     const callback = function (mutationsList, observer) {        
+    //         for (let mutation of mutationsList) {
+    //             if (mutation.type === "childList") {
+    //                 localStorage.setItem("todoes", ul_list.innerHTML);
+    //             } else if (mutation.type === "attributes") {
+    //                 localStorage.setItem("todoes", ul_list.innerHTML);
+    //             } else if (mutation.type === "subtree") {
+    //                 localStorage.setItem("todoes", ul_list.innerHTML);
+    //             } else if (mutation.type === "characterData") {
+    //                 localStorage.setItem("todoes", ul_list.innerHTML);
+    //             } 
+    //         }
+    //     };  
+    //     const observer = new MutationObserver(callback);
+    //     observer.observe(list, config);
+    }
+
+    removeLi(e) {
+        li.closest.remove();
+        this.toLocalStorage();
+    }
+}
+
+
+const task = new CreateNewTask();
+
+form.addEventListener("submit", e => {
     e.preventDefault();
-    
-    getValueOnSubmit();
-    
-    toLocalStorage();
-}
+    task.getValueOnSubmit();
+})
 
-function getValueOnSubmit() {   
-    
-    const formData = new FormData(form);
-    let value = Object.fromEntries(formData.entries());
-    if(!value.toList) return;
-    textInput.value = "";
-
-    createToDoLi(value);
-}
+window.addEventListener("load", e => {
+    task.reload();
+});
 
 
-function createToDoLi(value) {
-
-    let created_li = document.createElement("li");
-    ul_list.append(created_li);
-    let x = li_list.length;
-
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.id = `id${x}`;
-    created_li.append(checkbox);
-
-    let label = document.createElement('label')
-    label.htmlFor = `id${x}`;
-    label.append(document.createTextNode(value.toList));
-    created_li.append(label);
-
-    let delEl = document.createElement("span");
-    delEl.append(document.createTextNode("x"));
-    created_li.append(delEl);
-
-    delEl.addEventListener('click', removeLi => { 
-        created_li.remove();
-        toLocalStorage();
-
-    })
-    // ... to be continued
-}
-
-ul_list.addEventListener('change', changeHandler);
-
-function changeHandler() {
-    console.log(this.children);
-    toLocalStorage();
-}
-
-
-function toLocalStorage() {
-    let todoes = ul_list.innerHTML;
-    localStorage.setItem('todoes', todoes);
-}
-
-if(localStorage.getItem("todoes")) {
-    ul_list.innerHTML = localStorage.getItem("todoes");
-}
+ul_list.addEventListener('click', ev => {
+    task.changeHandler(ev);
+})
